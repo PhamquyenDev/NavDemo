97451d25792de108300146de15531b21846bb8c7
PhamquyenDev <78575413+PhamquyenDev@users.noreply.github.com>
2021-02-25 12:03:58 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React,  { Component } from 'react';
 
 import Navigation from "./src/navigation";
-
+//import Tabbar from './src/tabbar';
 
 export default function App() {
   return (
